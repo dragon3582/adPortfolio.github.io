70a12666a2fcd7ae100326d765de3aa7f1670b13
dragon3582 <adidonna35@knights.ucf.edu>
2016-07-12 22:40:13 -0400

    Added sketchfab integration
    integrated sketchfab to view 3d models.

---
+++ javascripts/embed3dStuff.js
@@ -0,0 +1,82 @@
+
+//take in content here
+	function embeddedContent(model, name, button, overPic)
+	{
+		
+		var modelInfo = {
+			modelURL: "hi",
+			nameURL: "lets",
+			nameText: "go",
+			modelOverview: "over"
+			
+		};
+		
+		//alert(modelInfo.nameText);
+		
+		var index;
+		
+		var modelArray = [];
+		
+		
+		var modelLinkArray = ["https://sketchfab.com/models/7f77833ff9b84733b4cb221bb37a2b60/embed",
+							  "https://sketchfab.com/models/c9d049309b1447ae84212e52922bc562/embed",
+							  "https://sketchfab.com/models/3c92609696144e9c8fc9ff78f6c0e2d5/embed"
+							  ];
+		//alert(modelLinkArray[0]);	  
+		var nameURLArray = ["https://sketchfab.com/models/7f77833ff9b84733b4cb221bb37a2b60?utm_medium=embed&utm_source=website&utm_campain=share-popup",
+							"https://sketchfab.com/models/c9d049309b1447ae84212e52922bc562?utm_medium=embed&utm_source=website&utm_campain=share-popup",
+							"https://sketchfab.com/models/3c92609696144e9c8fc9ff78f6c0e2d5?utm_medium=embed&utm_source=website&utm_campain=share-popup"
+							];
+		
+		var nameArray = ["Critical Mass Console",
+						 "Energy Reading Console",
+						 "Realistic Style Barrel"
+						 ];
+						 
+		var overviewPicArray = ["images/ConsoleOverview.png",
+								"images/ConsoleOverview.png",
+								"images/BarrelOverview.png"
+								];
+		
+		var theName;
+		var name_LinkReplacement;
+		var model_LinkReplacement;
+		
+		for(i = 0; i < modelLinkArray.length; i++)
+		{
+			modelArray[i] = {};
+			modelArray[i].modelURL = modelLinkArray[i];
+			modelArray[i].nameURL = nameURLArray[i];
+			modelArray[i].nameText = nameArray[i];
+			modelArray[i].modelOverview = overviewPicArray[i];
+		}
+		
+		for(i = 0; i < modelLinkArray.length; i++)
+		{
+			if(model == modelLinkArray[i])
+			{
+				index = i;
+			}
+		}
+		
+		if(button == "nextButton")
+		{
+			index++;
+			if(index >= modelArray.length)
+			{
+				index = 0;
+			}
+			//alert(modelArray[index].modelURL);
+			return modelArray[index];
+		}
+		else if(button == "previousButton")
+		{
+			index--;
+			if(index < 0)
+			{
+				index = modelArray.length-1;
+			}
+			return modelArray[index];
+		}
+		
+	}
